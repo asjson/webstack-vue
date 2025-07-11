4692762173a362b6c6fd9ee4b8ac46926c204d88
asjson <1432354487@qq.com>
2025-07-11 09:32:06 +0800

    导航站

---
+++ src/views/about.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="boxed-container">
+    <nav class="navbar horizontal-menu navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="navbar-brand">
+          <router-link to="/" class="logo">
+            <img src="../assets/images/logo_dark@2x.png" width="100%" alt="" class="hidden-xs">
+            <img src="../assets/images/logo@2x.png" width="100%" alt="" class="visible-xs">
+          </router-link>
+        </div>
+        <div class="navbar-mobile-clear"></div>
+      </div>
+    </nav>
+
+    <div class="page-container">
+      <div class="main-content" style="">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="panel panel-default">
+              <!-- 关于网站 -->
+              <h4 class="text-gray">关于网站</h4>
+              <div class="panel-body">
+                <div class="row">
+                  <div class="col-sm-12">
+                    <blockquote>
+                      <p>有一段时间我发现我的收藏夹越来越多，很难找到某个不常用的网站。
+                        所以就有了这个网站，这里收集的基本都是我自己浏览器收藏夹里的网站，我平时用的相对比较多的一些都在这里。</p>
+                      <br />
+                      <p>如果你也是设计师，如果你也正好喜欢设计，那希望这个网站能给你带来一些作用。</p>
+                    </blockquote>
+                  </div>
+                </div>
+                <!-- 关于站长 -->
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Footer from '../components/Footer.vue'
+
+export default {
+  name: 'About',
+  components: {
+    Footer
+  },
+  data() {
+    return {
+      authorImg: './assets/images/logos/viggoz.png'
+    }
+  }
+}
+</script>
+
+<style>
+</style>
